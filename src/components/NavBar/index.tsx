6be046aae2d1fbf328ee940373d65c7fb8d47f3b
ryanoliveir <ryandehallo229@gmail.com>
2023-12-13 00:30:24 -0300

    fix: NavBar

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,19 +1,21 @@
 import LogoutButton from "@components/LogoutButton";
 import { deleteCookie, getCookie } from "cookies-next";
+import Link from "next/link";
 import { useRouter } from "next/router";
 
 const NavBar = () => {
-
   const router = useRouter();
 
   const handleLogout = async () => {
     try {
-        deleteCookie('authorization');
-        router.push('/login');
-    } catch (error: any) {
-    }
-  }
+      deleteCookie("authorization");
+      router.push("/login");
+    } catch (error: any) {}
+  };
 
+  const isLinkActive = (href: any) => {
+    return router.pathname === href;
+  };
 
   return (
     <nav className="bg-white  border-gray-200 dark:bg-gray-900">
@@ -38,9 +40,9 @@ const NavBar = () => {
           >
             <path
               stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
               d="M1 1h15M1 7h15M1 13h15"
             />
           </svg>
@@ -48,50 +50,121 @@ const NavBar = () => {
         <div className="hidden w-full md:block md:w-auto" id="navbar-default">
           <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
             <li>
-              <a
+              <Link
                 href="/home"
-                className="block py-2 px-3 text-white rounded md:bg-transparent md:text-green-700 md:p-0 dark:text-white md:dark:text-green-500"
+                className={`block py-2 px-3 text-white rounded  ${
+                  isLinkActive("/home")
+                  ? "md:bg-transparent md:text-green-700 md:p-0 dark:text-white md:dark:text-green-500"
+                  : "hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-green-700 md:p-0 dark:text-white md:dark:hover:text-green-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                }`}
                 aria-current="page"
               >
                 Página Inicial
-              </a>
+              </Link>
             </li>
             <li>
-              <a
-                href="/courses"
-                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-green-700 md:p-0 dark:text-white md:dark:hover:text-green-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-              >
-                Cursos
-              </a>
-            </li>
-            <li>
-              <a
+              <Link
                 href="/painel"
-                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-green-700 md:p-0 dark:text-white md:dark:hover:text-green-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                className={`block py-2 px-3 text-white rounded  ${
+                  isLinkActive("/painel")
+                  ? "md:bg-transparent md:text-green-700 md:p-0 dark:text-white md:dark:text-green-500"
+                  : "hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-green-700 md:p-0 dark:text-white md:dark:hover:text-green-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                }`}
+                aria-current="page"
               >
                 Painel
-              </a>
+              </Link>
             </li>
             <li>
-              <a
+              <Link
+                href="/courses"
+                className={`block py-2 px-3 text-white rounded  ${
+                  isLinkActive("/courses")
+                  ? "md:bg-transparent md:text-green-700 md:p-0 dark:text-white md:dark:text-green-500"
+                  : "hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-green-700 md:p-0 dark:text-white md:dark:hover:text-green-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                }`}
+                aria-current="page"
+              >
+                Cursos
+              </Link>
+            </li>
+            <li>
+              <Link
                 href="/profile"
-                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-green-700 md:p-0 dark:text-white md:dark:hover:text-green-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                className={`block py-2 px-3 text-white rounded  ${
+                  isLinkActive("/profile")
+                  ? "md:bg-transparent md:text-green-700 md:p-0 dark:text-white md:dark:text-green-500"
+                  : "hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-green-700 md:p-0 dark:text-white md:dark:hover:text-green-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                }`}
+                aria-current="page"
               >
                 Perfil
-              </a>
+              </Link>
             </li>
           </ul>
         </div>
-        <LogoutButton handleClick={handleLogout}/>
+        <LogoutButton handleClick={handleLogout} />
         {/* <LogoutButton /> */}
       </div>
     </nav>
-    
   );
 };
 
-
 export default NavBar;
 
-
-
+{
+  /* <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+            <li>
+              <Link
+                href="/home"
+                passHref
+                className={`block py-2 px-3 rounded ${
+                  isLinkActive("/home")
+                    ? "text-green-700 bg-transparent md:text-white md:bg-green-500 dark:text-green-500 dark:bg-transparent dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                    : "text-white hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-green-700 dark:text-white md:dark:text-green-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                }`}
+              >
+                Página Inicial
+              </Link>
+            </li>
+            <li>
+              <Link href="/courses" passHref
+               
+                  className={`block py-2 px-3 rounded ${
+                    isLinkActive("/courses")
+                      ? "text-green-700 bg-transparent md:text-white md:bg-green-500 dark:text-green-500 dark:bg-transparent dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                      : "text-white hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-green-700 dark:text-white md:dark:text-green-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                  }`}
+                >
+                  Cursos
+               
+              </Link>
+            </li>
+            <li>
+              <Link href="/painel" passHref
+                
+                  className={`block py-2 px-3 rounded ${
+                    isLinkActive("/painel")
+                      ? "text-green-700 bg-transparent md:text-white md:bg-green-500 dark:text-green-500 dark:bg-transparent dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                      : "text-white hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-green-700 dark:text-white md:dark:text-green-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                  }`}
+                >
+                  Painel
+              
+              </Link>
+            </li>
+            <li>
+              <Link href="/profile" passHref
+                
+                  className={`block py-2 px-3 rounded ${
+                    isLinkActive("/profile")
+                      ? "text-green-700 bg-transparent md:text-white md:bg-green-500 dark:text-green-500 dark:bg-transparent dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                      : "text-white hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-green-700 dark:text-white md:dark:text-green-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                  }`}
+                >
+                  Perfil
+                
+              </Link>
+            </li>
+          </ul> */
+}
---
+++ src/pages/profile/index.tsx
@@ -0,0 +1,14 @@
+import NavBar from "@/components/NavBar";
+
+const ProfilePage = () => {
+  return (
+    <div className="flex flex-col h-screen">
+      <NavBar />
+      <section className="flex flex-1 bg-gray-900">
+
+      </section>
+    </div>
+  );
+};
+
+export default ProfilePage;
