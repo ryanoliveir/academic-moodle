b2a86f93c4d5bab2732dfaea45b9dea21eacc723
ryanoliveir <ryandehallo229@gmail.com>
2023-12-14 03:44:25 -0300

    fix: date format

--- src/utils/helper/index.ts
+++ src/utils/helper/index.ts
@@ -1,23 +1,16 @@
-export const maskDate = (date: Date) => {
-    if (!date) return ``;
-
-    const parsedDate = typeof date === 'string' ? new Date(date) : date;
-
-    const day = parsedDate.getDate();
-    const month = parsedDate.getMonth() + 1; // Months are zero-based, so add 1
-    const year = parsedDate.getFullYear();
-
-    const newDate = `${day}-${month}-${year}`
-  
-    return newDate;
-  };
+export const maskDate  = (date:any) => {
+  const d = new Date(date);
+  const month = `0${d.getUTCMonth() + 1}`.slice(-2);
+  const day = `0${d.getUTCDate()}`.slice(-2);
+  const year = d.getUTCFullYear();
+  return `${day}-${month}-${year}`;
+};
 
 
   export const  formatData = (dataString: string) =>{
-    // Dividir a string da data usando o caractere "-"
+   
     const parts = dataString.split('-');
   
-    // Mapear o mês para o nome completo
     const months = [
       'janeiro', 'fevereiro', 'março',
       'abril', 'maio', 'junho',
@@ -25,12 +18,11 @@ export const maskDate = (date: Date) => {
       'outubro', 'novembro', 'dezembro'
     ];
   
-    // Obter o dia, mês e ano da string dividida
+   
     const day = parts[0];
     const  month = months[parseInt(parts[1]) - 1];
     const year = parts[2];
   
-    // Retornar a data formatada
     return `${day} de ${month} de ${year}`;
   }
 
